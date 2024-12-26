c159dd99cf353b85229aab8206de5154db532a07
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-12-26 19:32:22 +0530

    setup react-app with vite, setup tailwind, and planing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold text-red-950'>
+        Helo Netflix Gpt
+      </div>
+       
+    </>
+  )
+}
+
+export default App
